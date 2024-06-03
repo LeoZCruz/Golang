ad5db6bbdc0ee0141fc00cb3fc46911181ae7deb
LeoZCruz <89984631+LeoZCruz@users.noreply.github.com>
2024-06-03 17:11:54 -0600

    Add: Data types

--- src/hello-world.go
+++ src/hello-world.go
@@ -57,4 +57,58 @@ func main() {
 	var baseMenor float64 = 5
 	var alturaTrapecio float64 = 7
 	fmt.Print("El area del trapecio es: ", (baseMayor+baseMenor)*alturaTrapecio/2, "\n")
+	//Valores Primitivos
+	var entero8 int8 = 127
+	var entero16 int16 = 32767
+	var entero32 int32 = 2147483647
+	var entero64 int64 = 9223372036854775807
+	fmt.Println(entero8, entero16, entero32, entero64)
+	//Valores Primitivos
+	var entero8Negativo int8 = -128
+	var entero16Negativo int16 = -32768
+	var entero32Negativo int32 = -2147483648
+	var entero64Negativo int64 = -9223372036854775808
+	fmt.Println(entero8Negativo, entero16Negativo, entero32Negativo, entero64Negativo)
+	//Valores Primitivos
+	var enteroSinSigno8 uint8 = 255
+	var enteroSinSigno16 uint16 = 65535
+	var enteroSinSigno32 uint32 = 4294967295
+	var enteroSinSigno64 uint64 = 18446744073709551615
+	fmt.Println(enteroSinSigno8, enteroSinSigno16, enteroSinSigno32, enteroSinSigno64)
+	//Valores Primitivos
+	var flotante32 float32 = 3.40282346638528859811704183484516925440e+38
+	var flotante64 float64 = 1.797693134862315708145274237317043567981e+308
+	fmt.Println(flotante32, flotante64)
+	//Valores Primitivos
+	var complejo64 complex64 = 1 + 2i
+	var complejo128 complex128 = 1 + 2i
+	fmt.Println(complejo64, complejo128)
+	//Valores Primitivos
+	var cadena string = "Hola Mundo"
+	fmt.Println(cadena)
+	//Valores Primitivos
+	var caracter byte = 'A'
+	fmt.Println(caracter)
+	//Valores Primitivos
+	var bandera bool = true
+	fmt.Println(bandera)
+	//Valores Primitivos
+	var error error = nil
+	fmt.Println(error)
+	//Valores Primitivos
+	var puntero *int = &x
+	fmt.Println(puntero)
+	//Valores Primitivos
+	var arreglo [3]int
+	fmt.Println(arreglo)
+	//Valores Primitivos
+	var slice []int
+	fmt.Println(slice)
+	//Valores Primitivos
+	var mapa map[string]int
+	fmt.Println(mapa)
+	//Valores Primitivos
+	var canal chan int
+	fmt.Println(canal)
+
 }
